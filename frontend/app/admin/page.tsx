0012c72c8b667eb3873532d8dd6f3a3fa3d6a0db
mgtsampayan <mulawin0921@gmail.com>
2025-01-15 21:50:17 +0800

    Refactor AdminDashboard component for improved formatting

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -4,12 +4,12 @@ import Auth from '../auth/auth';
 
 
 export default function AdminDashboard() {
-return (
-    <Auth allowedRoles={['admin']}>
-        <div>
-        <h1>Admin Dashboard</h1>
-        <p> Welcome to the Admin Dashboard </p>
-        </div>
-    </Auth>
-);
+    return (
+        <Auth allowedRoles={['admin']}>
+            <div>
+                <h1>Admin Dashboard</h1>
+                <p> Welcome to the Admin Dashboard </p>
+            </div>
+        </Auth>
+    );
 }
