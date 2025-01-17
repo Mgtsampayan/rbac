cb03ea35589f2359c9fc254b42fd56d2a604aadc
mgtsampayan <mulawin0921@gmail.com>
2025-01-17 13:25:00 +0800

    Remove commented-out code from Dashboard component in page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,3 @@
-// 'use client';
-// import Auth from './auth/auth';
-
-
-// export default function Dashboard() {
-//   return (
-//       <Auth allowedRoles={['admin', 'student', 'faculty', 'registrar', 'accounting', 'hr', 'parent']}>
-//         <div>
-//           <h1>Dashboard</h1>
-//           <p> Welcome to the </p>
-//         </div>
-//       </Auth>
-//   );
-// }
-
 'use client';
 
 import { useEffect } from 'react';
